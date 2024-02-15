6e115fc6c73d0b3e2b307f4251dcd8fff640b184
DaveKirby111 <112968620+DaveKirby111@users.noreply.github.com>
2024-02-15 14:18:47 -0600

    working on layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,17 +1,18 @@
 import React from "react";
+import "../../App.css";
 
 import Nav from "../Nav";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <header>
-        <h1>office tools</h1>
+        <h1>common tools</h1>
       </header>
 
       <Nav />
 
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
 
       <footer>
         <p>david kirby</p>
